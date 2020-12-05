f52df5098d5c552694c0be9cae53aef846de7b7a
gagel1 <44709880+gagel1@users.noreply.github.com>
2020-12-04 22:46:55 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,87 @@
+// This socket connects to root path
+const socket = io('/')
+const videoGrid = document.getElementById('video-grid')
+const msgContainer = document.getElementById('message-container')
+const msgForm = document.getElementById('send-container')
+const msgInput = document.getElementById('message-input')
+
+appendMessage('You joined')
+
+msgForm.addEventListener('submit', e => {
+  e.preventDefault()
+  const message = msgInput.value
+  appendMessage(`You: ${message}`)
+  socket.emit('send-message', message)
+  msgInput.value = ''
+})
+
+// Basic Peer configurations for dynamic id generation with the uuid package
+const myPeer = new Peer(undefined, {
+  host: '/',
+  port: '3001'
+})
+
+// User's personal video element. We mute them because we don't want to them to hear themselves
+const myVideo = document.createElement('video')
+myVideo.muted = true
+const peers = {}
+navigator.mediaDevices.getUserMedia({
+  video: true,
+  audio: true
+}).then(stream => {
+  addVideoStream(myVideo, stream)
+
+  myPeer.on('call', call => {
+    call.answer(stream)
+    const video = document.createElement('video')
+    call.on('stream', userVideoStream => {
+      addVideoStream(video, userVideoStream)
+    })
+    console.log('Anotha one!!')
+  })
+
+  socket.on('user-connected', name => {
+    appendMessage(`${name} connected`)
+    connectToNewUser(name, stream)
+  })
+})
+
+socket.on('chat-message', data => {
+  appendMessage(`${data.name}: ${data.message}`)
+})
+
+socket.on('user-disconnected', name => {
+  appendMessage(`${name} disconnected`)
+  if (peers[name]) peers[name].close()
+})
+
+myPeer.on('open', id => {
+  socket.emit('join-room', ROOM_ID, id)
+})
+
+function connectToNewUser(userId, stream) {
+  const call = myPeer.call(userId, stream)
+  const video = document.createElement('video')
+  call.on('stream', userVideoStream => {
+    addVideoStream(video, userVideoStream)
+  })
+  call.on('close', () => {
+    video.remove()
+  })
+
+  peers[userId] = call
+}
+
+function addVideoStream(video, stream) {
+  video.srcObject = stream
+  video.addEventListener('loadedmetadata', () => {
+    video.play()
+  })
+  videoGrid.append(video)
+}
+
+function appendMessage(message) {
+  const messageElement = document.createElement('div')
+  messageElement.innerText = message
+  msgContainer.append(messageElement)
+}
--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 const express = require('express')
 const app = express()
 const server = require('http').Server(app)
@@ -9,7 +8,7 @@ const users = {}
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
-// redirect to the user to the room dyanamically
+// Redirect the user to the room dynamically
 app.get('/', (req, res) => {
   res.redirect(`/${uuidV4()}`)
 })
@@ -19,30 +18,24 @@ app.get('/:room', (req, res) => {
   res.render('room', { roomId: req.params.room })
 })
 
-// handle the connection of the software with the socket.io package
-// listen to events under the following conditions: once a chap connects
+// Handle the connection of the software with the socket.io package
+// Listen to events under the following conditions: once a chap connects
 io.on('connection', socket => {
   socket.on('join-room', (roomId, name) => {
     socket.join(roomId)
     users[socket.id] = `${name}`
     socket.to(roomId).broadcast.emit('user-connected', users[socket.id])
-  })
 
-  // run the user-connected function if socket detects new connection
-  //socket.on('new-user', name => {
-    
-  //})   
-  //socket.broadcast.emit('user-connected', users[socket.id])
+    // Run disconnect function if user leaves room
+    socket.on('disconnect', () => {
+      socket.broadcast.emit('user-disconnected', users[socket.id])
+      delete users[socket.id]
+    })
 
-  socket.on('send-message', message => {
-    socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] })
-  })
-
-  // run disconnect function if user leaves room
-  socket.on('disconnect', () => {
-    socket.broadcast.emit('user-disconnected', users[socket.id])
-    //socket.broadcast.emit('user-disconnected', users[socket.id])
-    delete users[socket.id]
+    socket.on('send-message', message => {
+      socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] })
+    })
   })
 })
+
 server.listen(3000)
